4c3a04e62d28750364388046723d81887ec1b5c8
Nishant Chalaune <nishantchalaune11@gmail.com>
2026-04-26 23:07:00 -0700

    Fix DATABASE_URL protocol for postgres

--- backend/db.py
+++ backend/db.py
@@ -3,6 +3,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./momentum.db")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 
 
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}

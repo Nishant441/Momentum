b462af8e7e438b4bdf4f46574905c679c66ef89c
Nishant Chalaune <nishantchalaune11@gmail.com>
2026-04-26 23:25:51 -0700

    Update CORS allowed origins

--- backend/main.py
+++ backend/main.py
@@ -16,17 +16,8 @@ init_db()
 
 ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-    "http://127.0.0.1:5175",
-    "http://localhost:4173",
+    "https://momentum-puce-delta.vercel.app"
 ]
-
-frontend_url = os.getenv("FRONTEND_URL")
-if frontend_url:
-    ALLOWED_ORIGINS.append(frontend_url)
 
 app.add_middleware(
     CORSMiddleware,
